e2d820d0022afff79e6631abc9666b9165cb98a3
Prestobd04 <prestobd04@gmail.com>
2026-03-03 21:39:36 -0500

    Add the get ping endpoint confirming connection to postgres database also setup the database in sql alchemy

--- apps/api/app/api/routes/health.py
+++ apps/api/app/api/routes/health.py
@@ -1,7 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from sqlalchemy import text
+
+from app.db import get_db
 
 router = APIRouter()
 
 @router.get("/health")
 def health():
     return {"status": "ok"}
+
+@router.get("/db/ping")
+def db_ping(db: Session = Depends(get_db)):
+    db.execute(text("SELECT 1"))
+    return {"status": "ok"}
+    
